4596ba30a9be6242801b46cbf22662ed6ae6cbff
Arnab <arnpoddar@deloitte.com>
2022-06-02 12:21:24 +0530

    First Update

--- models/example/milestone1_2.sql
+++ models/example/milestone1_2.sql
@@ -1,9 +1,9 @@
 {{ config(materialized='table') }}
 
-select nav_date, t4.code as code, t4.maximum from "ANALYTICS"."DBT"."NAVHISTORY" t3 join
+select nav_date, table4.code as code, table4.maximum from "ANALYTICS"."DBT"."NAVHISTORY" table3 join
 
-(select t1.code as code, max(t2.nav) as maximum from "ANALYTICS"."DBT"."MUTUALFUND" t1 join
+(select table1.code as code, max(table2.nav) as maximum from "ANALYTICS"."DBT"."MUTUALFUND" table1 join
 
- "ANALYTICS"."DBT"."NAVHISTORY" t2 on t1.code=t2.code group by t1.code) t4
+ "ANALYTICS"."DBT"."NAVHISTORY" table2 on table1.code=table2.code group by table1.code) table4
 
-where t3.code=t4.code and t3.nav=t4.maximum
+where table3.code=table4.code and table3.nav=table4.maximum
